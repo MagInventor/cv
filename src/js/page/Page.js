b279ea20bdb0d60bf5cc7297ba1a1d6f3b848016
dreamtogostar <dreamerstargazer@yandex.ru>
2022-07-29 17:03:51 +0300

    feat: add skills

--- src/js/page/Page.js
+++ src/js/page/Page.js
@@ -12,10 +12,13 @@ class Page {
   }
 
   createAside() {
-    console.log(this.person.contacts);
     this.contacts = this.person.contacts.map(el => `<li><h4>${el[0]}</h4><p>${el[1]}</p></li>`);
     this.contacts = `<ul>${this.contacts.join('')}</ul>`
-    document.querySelector('.contacts .container').innerHTML = this.contacts;
+    document.querySelector('.contacts .aside-container').innerHTML = this.contacts;
+
+    this.skills = this.person.skills[1].map(el => `<li>${el}</li>`).join('');
+    this.skills = `<ul>${this.skills}</ul>`;
+    document.querySelector('.skills .aside-container').innerHTML = this.skills;
   }
 
   createMain() {
