095cb743546228b0091d1a14d6f0e47d812cfdd2
dreamtogostar <dreamerstargazer@yandex.ru>
2022-07-29 10:46:39 +0300

    feat: add create section

--- src/js/components/main/Main.component.js
+++ src/js/components/main/Main.component.js
@@ -2,12 +2,22 @@ class Main {
   constructor() {
     this.main = document.createElement('main');
     this.main.classList.add('field', 'field-main');
+    this.about = this.createSection('about');
+    this.experience = this.createSection('experience');
+    this.education = this.createSection('education');
+  }
+
+  createSection(nameSection) {
+    this.section = document.createElement('section');
+    this.section.classList.add(nameSection);
     this.container = document.createElement('div');
     this.container.classList.add('container');
+    this.section.append(this.container);
+    return this.section;
   }
 
   addMain() {
-    this.main.append(this.container);
+    this.main.append(...[this.about, this.experience, this.education]);
     return this.main;
   }
 }
